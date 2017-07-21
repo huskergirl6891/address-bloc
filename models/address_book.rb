81e7c00beb0b58cbf98d9e04bc2f3f3df19fbbf1
Carissa Castro <carissabcastro@gmail.com>
2017-07-21 17:24:51 -0500

    Assignment 4 models final updates

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,26 +9,26 @@ class AddressBook
 
   def add_entry(name, phone_number, email)
     index = 0
-    entries.each do |entry|
+    @entries.each do |entry|
       if name < entry.name
         break
       end
       index += 1
     end
 
-    entries.insert(index, Entry.new(name, phone_number, email))
+    @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
   def remove_entry(name, phone_number, email)
     index = 0
-    entries.each do |entry|
+    @entries.each do |entry|
       if name == entry.name && phone_number == entry.phone_number && email == entry.email
         break
       end
       index += 1
     end
 
-    entries.delete_at(index)
+    @entries.delete_at(index)
   end
 
 end
