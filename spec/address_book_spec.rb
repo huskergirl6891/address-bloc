74b9502e0d5fd27d70a129054b827c29de372c61
Carissa Castro <carissabcastro@gmail.com>
2017-07-21 17:17:22 -0500

    Assignment 4 models complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe AddressBook do
     end
   end
 
-  describe "#add_entries" do
+  describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -35,4 +35,12 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    it "removes only one entry to the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(0)
+    end
+  end
 end
